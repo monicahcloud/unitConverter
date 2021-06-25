73f7a4ec546bd34390da8e648caf5a9ea01c9112
Monicah Cloud <monicahcloud@gmail.com>
2021-06-25 17:55:55 -0400

    Final Project

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Converter {
 
+	static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Metric Conversion!\nWhat is your first name?");
-		Scanner input = new Scanner(System.in);
+		
 		String name = input.nextLine();
 		System.out.println("Hello " + name);
-		
+			
 		boolean anotherConversion = true;
 		
 		while(anotherConversion) {
@@ -25,43 +27,61 @@ public class Converter {
 		
 		switch(userSelection) {
 			case ( 1): {
-				System.out.println("How many km are you converting to m?");
-				int km = input.nextInt();
-				int kiloToMeters = km * 1000;
-				System.out.println("That's an easy one! " + km + "km is equal to " + kiloToMeters + "m");
-				System.out.println("");
+				kilometers();
 				}
 			break;
 			case ( 2): {
-				System.out.println("How many m are you converting to cm?");
-				int m = input.nextInt();
-				int metersToCentimeters = m * 100;
-				System.out.println("Good one! " + m + "m is equal to " + metersToCentimeters + "cm");
-				System.out.println("");
+				meters();
 				}
 			break;
 			case ( 3) :{
-				System.out.println("How many cm are you converting to mm?");
-				int cm = input.nextInt();
-				int centimetersToMillimeters = cm * 10;
-				System.out.println("Piece of cake" + cm + "cm is equal to " + centimetersToMillimeters + "mm");
-				System.out.println("");
+				centimeters();
 				}
 			break;
 			default: {
-				System.out.println("You did not make a valid selection...\nPlease try again: ");
-				System.out.println("");
+				invalid();
 				}
-		}
-		}
+			}//end of switch statement
+		}//end of if statement
 		else if (answer.equals("no")) {
 			anotherConversion = false;
 		
 		System.out.println("See ya next time!");
-		}
-		}
-		}
+		
+				}//end of else if statement
+			}//end of while loop
+		input.close();
+		}//end of main method
+	
+	public static void kilometers() {
+		System.out.println("How many km are you converting to m?");
+		int km = input.nextInt();
+		int kiloToMeters = km * 1000;
+		System.out.println("That's an easy one! " + km + "km is equal to " + kiloToMeters + "m");
+		System.out.println("");
 	}
+	
+	public static void meters() {
+		System.out.println("How many m are you converting to cm?");
+		int m = input.nextInt();
+		int metersToCentimeters = m * 100;
+		System.out.println("Good one! " + m + "m is equal to " + metersToCentimeters + "cm");
+		System.out.println("");
+	}
+	
+	public static void centimeters() {
+		System.out.println("How many cm are you converting to mm?");
+		int cm = input.nextInt();
+		int centimetersToMillimeters = cm * 10;
+		System.out.println("Piece of cake" + cm + "cm is equal to " + centimetersToMillimeters + "mm");
+		System.out.println("");
+	}
+	public static void invalid() {
+		System.out.println("You did not make a valid selection...\nPlease try again: ");
+		System.out.println("");
+	}
+	
+	}//end of class
 		
 
 
